6fe122447185cabe0cc7a03f688877761d51b2f6
H’R <2811668688@qq.com>
2024-02-02 15:22:41 +0800

    init demo page

---
+++ helper.js
@@ -0,0 +1,145 @@
+function createAudioHTML(path) {
+  return '<audio controls controlslist="nodownload" class="px-1"> <source src=' +
+      path +
+      ' type="audio/wav">Your browser does not support the audio element.</audio>';
+}
+
+
+function generateExampleRow(table_row, base_path, filename_ext, col_offset) {
+  for (var i = 0; i < filename_ext.length; i++) {
+    let p = base_path + filename_ext[i];
+    let cell = table_row.cells[col_offset + i];
+    // console.log(table_row.cells.length)
+    if (p.endsWith('txt')) {
+      var req = new XMLHttpRequest();
+      req.open("GET", p, false);
+      req.send(null);
+      cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
+    } else {
+      cell.innerHTML = cell.innerHTML + createAudioHTML(p);
+    }
+  }
+}
+
+function generateExampleRowCross(table_row, base_path, filename_ext, col_offset) {
+  for (var i = 0; i < filename_ext.length; i++) {
+    let p = base_path + filename_ext[i];
+    if (i === 1) {
+      col_offset = col_offset + 1
+    } 
+
+    let cell = table_row.cells[col_offset + i];
+    // console.log(table_row.cells.length)
+    if (p.endsWith('txt')) {
+      var req = new XMLHttpRequest();
+      req.open("GET", p, false);
+      req.send(null);
+      cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
+    } else {
+      cell.innerHTML = cell.innerHTML + createAudioHTML(p);
+    }
+  }
+}
+
+function generateEmotion(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_gt.wav', '_prompt.wav', '_yourtts.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 5; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/emotion/' + i, ext, 0);
+  }
+}
+
+function generateStyle(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_gt.wav', '_prompt.wav', '_yourtts.wav', '_base.wav','_medium.wav','_large.wav'];
+  console.log(12347)
+  for (var i = 0; i < 7; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/style/' + i, ext, 0);
+  }
+}
+
+function generateSpeed(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_all.txt', '_gt.wav', '_base.wav','_medium.wav','_large.wav'];
+  console.log(12345)
+  for (var i = 0; i < 6; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/speed/' + i, ext, 0);
+  }
+}
+
+function generateEnergy(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_all.txt', '_gt.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 6; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/energy/' + i, ext, 0);
+  }
+}
+
+function generateRegionAdd(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_raw.txt', '_new.txt', '_gt.wav', '_se.wav', '_a3t.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/Region-based/add/' + i, ext, 0);
+  }
+}
+
+function generateRegionDelete(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_raw.txt', '_new.txt', '_gt.wav', '_se.wav', '_a3t.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/Region-based/delete/' + i, ext, 0);
+  }
+}
+
+function generateRegionReplace(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_raw.txt', '_new.txt', '_gt.wav', '_se.wav', '_a3t.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/Region-based/replace/' + i, ext, 0);
+  }
+}
+
+function generateFreeRegionAdd(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_raw.txt', '_new.txt', '_gt.wav', '_se.wav', '_a3t.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/Region-free/add/' + i, ext, 0);
+  }
+}
+
+function generateFreeRegionDelete(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_raw.txt', '_new.txt', '_gt.wav', '_se.wav', '_a3t.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/Region-free/delete/' + i, ext, 0);
+  }
+}
+
+function generateFreeRegionReplace(tableId) {
+  let table = document.getElementById(tableId);
+  let ext = ['_txt.txt', '_raw.txt', '_new.txt', '_gt.wav', '_se.wav', '_a3t.wav', '_base.wav','_medium.wav','_large.wav'];
+
+  for (var i = 0; i < 4; i++) {
+    generateExampleRow(table.rows[1 + i], 'data/Region-free/replace/' + i, ext, 0);
+  }
+}
+
+generateEmotion('emotion-conversion-table')
+generateStyle('style-conversion-table')
+generateSpeed('Speed-conversion-table')
+generateEnergy('Energy-conversion-table')
+generateRegionAdd('add-region-based-table')
+generateRegionDelete('delete-region-based-table')
+generateRegionReplace('replace-region-based-table')
+generateFreeRegionAdd('add-region-free-table')
+generateFreeRegionDelete('delete-region-free-table')
+generateFreeRegionReplace('replace-region-free-table')
+
+
